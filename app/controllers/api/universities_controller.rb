3bd54b4017c0b40c1150da3bfcdbe36d6eea0d60
Jeneen <jeneen.lemar@gmail.com>
2020-02-04 21:04:30 -0500

    adds app initial files

---
+++ app/controllers/api/universities_controller.rb
@@ -0,0 +1,10 @@
+class Api::UniversitiesController < ApplicationController
+
+  def index
+    @response = HTTP.get("http://universities.hipolabs.com/")
+    # @universities = response.parse["body"]
+    render "index.json.jb"
+  end
+
+end
+#source http://universities.hipolabs.com/search?name=middle
